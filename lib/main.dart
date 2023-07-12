e60096dc1f40804ea10558b481a70eda0be7e422
İsmail <olgunerismail2@gmail.com>
2023-07-12 12:19:41 +0300

    ekran oluşturma ve rota tanımlama

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => GirisEkrani(),
+        "ProfilSayfasiRotasi": (context) => ProfilEkrani(),
+      },
+    );
+  }
+}
+
+class GirisEkrani extends StatefulWidget {
+  const GirisEkrani({super.key});
+
+  @override
+  State<GirisEkrani> createState() => _GirisEkraniState();
+}
+
+class _GirisEkraniState extends State<GirisEkrani> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ProfilEkrani extends StatefulWidget {
+  const ProfilEkrani({super.key});
+
+  @override
+  State<ProfilEkrani> createState() => _ProfilEkraniState();
+}
+
+class _ProfilEkraniState extends State<ProfilEkrani> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
