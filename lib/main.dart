09afbd1f059cd5c70be35fae5ad6510591050270
İsmail <olgunerismail2@gmail.com>
2023-07-12 14:12:46 +0300

    yanlış girildiğinde uyarı verilmesi

--- lib/main.dart
+++ lib/main.dart
@@ -28,25 +28,49 @@ class _GirisEkraniState extends State<GirisEkrani> {
   TextEditingController t1 = TextEditingController();
   TextEditingController t2 = TextEditingController();
   girisYap() {
-    Navigator.pushNamed(context, "/ProfilSayfasiRotasi",
-        arguments: VeriModeli(kullaniciAdi: t1.text, sifre: t2.text));
+    if (t1.text == "ismail" && t2.text == "1234") {
+      Navigator.pushNamed(context, "/ProfilSayfasiRotasi",
+          arguments: VeriModeli(kullaniciAdi: t1.text, sifre: t2.text));
+    } else {}
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Yanlış kullanıcı adı veya şifre"),
+          content: new Text("Lütfen giriş bilgilerinizi gözden geçirin."),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text("Kapat"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Giriş Ekrani")),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: t1,
-            ),
-            TextFormField(
-              controller: t2,
-            ),
-            ElevatedButton(onPressed: girisYap, child: Text("Giriş Yap")),
-          ],
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(100),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(hintText: "Kullanıcı Adı"),
+                controller: t1,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Şifre"),
+                controller: t2,
+              ),
+              ElevatedButton(onPressed: girisYap, child: Text("Giriş Yap")),
+            ],
+          ),
         ),
       ),
     );
